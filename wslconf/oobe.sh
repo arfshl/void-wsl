2df97ab84943773b5630042487cd850f7fef4a8d
arfshl <98870352+arfshl@users.noreply.github.com>
2026-04-22 12:07:58 +0700

    Update oobe.sh

--- wslconf/oobe.sh
+++ wslconf/oobe.sh
@@ -2,7 +2,7 @@
 
 set -ue
 
-#DEFAULT_GROUPS='adm,cdrom,sudo,dip,plugdev'
+DEFAULT_GROUPS='adm,cdrom,plugdev,wheel'
 DEFAULT_UID='1000'
 
 echo 'Please create a default UNIX user account. The username does not need to match your Windows username.'
@@ -19,15 +19,17 @@ while true; do
   read -p 'Enter new UNIX username: ' username
 
   # Create the user
-  if /usr/sbin/adduser --uid "$DEFAULT_UID" --gecos ''  "$username"; then
+  if /usr/sbin/useradd --uid "$DEFAULT_UID" -D "$username"; then
 
-    if /usr/sbin/addgroup "$username" wheel; then
+    if /usr/sbin/usermod -aG "$username" $DEFAULT_GROUPS; then
       break
     else
-      /usr/sbin/deluser "$username"
+      /usr/sbin/userdel "$username"
     fi
   fi
 done
+
+chsh -s bash "$username"
 
 cat > /etc/sudoers.d/wsluser << EOF
 %wheel ALL=(ALL:ALL) ALL
